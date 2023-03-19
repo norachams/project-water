01ab9ea645b78372ff98e4c2624fff813a9e95b6
Nora Chamseddin <nchamseddin@gmail.com>
2023-03-19 16:51:04 +0000

    Add system service to redeploy script

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -14,6 +14,16 @@ python -m venv python3-virtualenv
 source python3-virtualenv/bin/activate
 pip install -r requirements.txt
 
+# Step: Copy service file to systemd directory
+sudo cp myportfolio.service /etc/systemd/system/
+
+# Step: Reload systemd daemon
+sudo systemctl daemon-reload
+
+# Step: Enable and start the service
+sudo systemctl enable myportfolio.service
+sudo systemctl start myportfolio.service
+
 # Step: Start Flask server in a new detached Tmux session
 cd ..
 tmux new -s myflasksession 'cd project-water && flask run --host=0.0.0.0'
