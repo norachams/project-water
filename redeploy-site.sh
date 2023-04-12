dd6f806c12f43d5f6ec38fdb2276cbecbd4dadb0
nora chams <nchamseddin@gmail.com>
2023-04-12 03:08:29 -0400

    added redeploy

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -1,30 +1,13 @@
 #!/bin/bash
 
-# Step: Kill all existing tmux sessions
-tmux kill-server
-
 # Step: cd into project folder
-cd project-water
+cd /path/to/your/project-folder
 
 # Step: Get latest changes from main branch on GitHub
 git fetch && git reset origin/main --hard
 
-# Step: Activate python virtual environment and install dependencies
-python -m venv python3-virtualenv
-source python3-virtualenv/bin/activate
-pip install -r requirements.txt
+# Step: Spin containers down to prevent out of memory issues on the VPS instance
+docker-compose -f docker-compose.prod.yml down
 
-# Step: Copy service file to systemd directory
-sudo cp myportfolio.service /etc/systemd/system/
-
-# Step: Reload systemd daemon
-sudo systemctl daemon-reload
-
-# Step: Enable and start the service
-sudo systemctl enable myportfolio.service
-sudo systemctl start myportfolio.service
-
-# Step: Start Flask server in a new detached Tmux session
-cd ..
-tmux new -s myflasksession 'cd project-water && flask run --host=0.0.0.0'
-echo "Reached"
+# Step: Build and start containers in detached mode
+docker-compose -f docker-compose.prod.yml up -d --build
