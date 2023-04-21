c693a62de7a62a18dafec5f2333c7432cd59b921
nora chams <nchamseddin@gmail.com>
2023-04-21 10:35:19 -0400

    errors

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Step: cd into project folder
-cd /path/to/your/project-folder
+cd project-water
 
 # Step: Get latest changes from main branch on GitHub
 git fetch && git reset origin/main --hard
